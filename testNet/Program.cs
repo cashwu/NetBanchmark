3874f7828cb8db7bc6a88e0788b4c7d7dd237d8a
cash <cash@cashwu.com>
2020-08-22 15:45:32 +0800

    init

---
+++ testNet/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Security.Cryptography;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace testNet
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<Md5VsSha512>();
+        }
+        
+        public class Md5VsSha512
+        {
+            private const int N = 10000;
+            private readonly byte[] data;
+
+            private readonly SHA512 sha512 = SHA512.Create();
+            private readonly MD5 md5 = MD5.Create();
+
+            public Md5VsSha512()
+            {
+                data = new byte[N];
+                new Random(42).NextBytes(data);
+            }
+
+            [Benchmark]
+            public byte[] Sha512() => sha512.ComputeHash(data);
+
+            [Benchmark]
+            public byte[] Md5() => md5.ComputeHash(data);
+        }
+    }
+}
---
+++ testNet/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("testNet")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("testNet")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("AD182066-9D9E-444C-B183-4DB6680C1AF8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
